f8c96704d788a1612f0d172d945d1d91a4213e38
Abhi <neemnit@gmail.com>
2024-01-10 11:30:42 +0530

    superhunter app completed fixed some things

--- superhero.js
+++ superhero.js
@@ -1,43 +1,34 @@
 let favouritesSuperHero
-let favourites=[]
-const addToFavorites=(data)=>{
+let favourites = []
+const addToFavorites = (data) => {
     if (favourites.length === 0) {
         favourites = [data, ...favourites];
-    } else {
-        if (!favourites.some(item => item.id === data.id)) {
-            favourites = [data, ...favourites];
-        }
+    } else if (!favourites.some(item => item.id === data.id)) {
+        favourites = [data, ...favourites];
     }
+    localStorage.setItem("favourites", JSON.stringify(favourites))
     
-
-//    favourites=favourites.filter((favourite)=>{
-//     if(favourite.id!==data.id){
-//         return favourite
-//     }
-//     // else{
-//     //     return data
-//     // }
-    
-//    })
-   console.log(favourites)
-   
-    localStorage.setItem("favourites",JSON.stringify(favourites))
-     
 }
-favouritesSuperHero=JSON.parse(localStorage.getItem("favourites"))
+favouritesSuperHero = JSON.parse(localStorage.getItem("favourites"))
 
-const removeFromFavorites=(data)=>{
-    console.log("kukmkjkl",data)
-    const filterData=favouritesSuperHero.filter((dat)=>{
-        return dat.id!==data.id
+const removeFromFavorites = (data) => {
+    
+    const filterData = favouritesSuperHero.filter((dat) => {
+        return dat.id !== data.id
     })
-    localStorage.setItem("favourites",JSON.stringify(filterData))
-    favouritesSuperHero=filterData
-    console.log(favouritesSuperHero)
+    localStorage.setItem("favourites", JSON.stringify(filterData))
+    favouritesSuperHero = filterData
+
+
+}
+const viewPage = (data) => {
+
+    favouritesSuperHero=JSON.parse(localStorage.getItem("favourites"))
+    const findFavour = favouritesSuperHero.find((dat) => {
+
+        return dat.id == data.id
+    })
     
-    }
-    const viewPage=(data)=>{
-        console.log(data,"htid")
-    }
-    
-    
+    return findFavour
+}
+
